52d83b1540a0588a7e5c015d8ccf05f3df7757ea
Liv Nelson <livnelson@me.com>
2022-10-20 16:34:22 -0700

    complete labs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const loadDogs_URL = 'https://dog.ceo/api/breeds/image/random/4'
+
+//first fetch and load the dog images to the page//
+
+const loadDogs = () => {
+    fetch(loadDogs_URL)
+    .then((response) => response.json())
+    .then((dogs) => {
+        // displayDogs(dogs)
+        console.log(dogs)
+    })
+    
+}
+
+function displayDogs(dogs) {
+    dogs.forEach(dogs)
+}
+
+
+// const dogContainer = document.getElementById('dog-image-container')
+// dogContainer.append(dogs)
+
+
+
+
+
+//then load the dog images to the page//
+
+const init = (dogs) => {
+    loadDogs()
+    displayDogs(dogs)
+}
+
+init()
